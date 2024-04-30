47c341826f93f5bf26d80d87bdb67aea4c61b807
Daitzyx <nathanrtd01@gmail.com>
2024-04-29 23:22:11 -0300

    refact: teste

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 console.log("TESTE manu 2");
+<<<<<<< HEAD
 console.log("Subindo novo TESTE nathan 14");
+=======
+console.log("Subindo novo TESTE nathan 15");
+>>>>>>> f2f5315 (teste)
 
 console.log("REECEEEBAAA, testeando")
