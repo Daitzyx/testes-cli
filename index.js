8d719dd5994a9a2d17efa9e940adf163cc077077
Daitzyx <nathanrtd01@gmail.com>
2024-04-29 23:16:51 -0300

    teste

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 console.log("TESTE manu 2");
-<<<<<<< HEAD
-console.log("Subindo novo TESTE nathan 14");
-=======
 console.log("Subindo novo TESTE nathan 15");
->>>>>>> f2f5315 (teste)
 
 console.log("REECEEEBAAA, testeando")
