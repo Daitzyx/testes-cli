cd5b9f7b70780f7771cca76c9a3facae1a962f2d
Nathan <87097402+Daitzyx@users.noreply.github.com>
2024-04-29 23:28:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 console.log("TESTE manu 2");
-<<<<<<< HEAD
-console.log("Subindo novo TESTE nathan 16");
-=======
-console.log("Subindo novo TESTE nathan 17");
->>>>>>> fbad3ff (teste)
+console.log("Subindo novo TESTE nathan 18");
 
 console.log("REECEEEBAAA, testeando")
