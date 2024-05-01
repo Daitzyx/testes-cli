e5d27c08a556eab4a51a499b28c353b27de0e404
Daitzyx <nathanrtd01@gmail.com>
2024-05-01 17:34:50 -0300

    tes

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 console.log("TESTE manu 2");
-<<<<<<< HEAD
-console.log("Subindo novo TESTE nathan 99");
-=======
 console.log("Subindo novo TESTE nathan 100");
->>>>>>> c259ba6 (tes)
 
 console.log("REECEEEBAAA, testeando")
