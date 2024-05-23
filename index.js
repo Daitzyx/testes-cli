0e49d1d4af8eb654288de60b7ebf2d1ca16025fd
Nathan <87097402+Daitzyx@users.noreply.github.com>
2024-05-23 00:54:42 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ console.log("TESTE manu 2");
 console.log("Subindo novo TESTE nathan 156");
 console.log("TESTE 10")
 
-console.log("REECEEEBAAA, testeando")
+console.log("REECEEEBAAA, testeando 2")
 console.log("REECEEEBAAA, testeando 1")
