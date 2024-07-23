a617f2ab8b99302134c724e95a8e9f274aee8a25
Daitzyx <nathanrtd01@gmail.com>
2024-07-23 18:22:00 -0300

    refactor: remove files

--- gui.js
+++
@@ -1,2 +0,0 @@
-console.log("guionei")
-console.log("guionei5")
--- index.js
+++
@@ -1,9 +0,0 @@
-console.log("TESTE manu 2");
-console.log("Subindo novo TESTE nathan 156");
-console.log("TESTE 11")
-
-
-console.log("Testando update status line message 1")
-console.log("REECEEEBAAA, testeando 8")
-console.log("REECEEEBAAA, testeando 10")
-console.log("REECEEEBAAA, testeando 58")
