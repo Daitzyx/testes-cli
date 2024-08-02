a1a229f357bc0f94db28b07b0079c37e7065dae0
Nathan <87097402+Daitzyx@users.noreply.github.com>
2024-08-02 13:59:20 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,14 +2,5 @@ function log(text){
   console.log(text);
 }
 
-function fromLocal(text){
-  console.log(text);
-}
 
-fromLocal("Essa é uma funcão criada local");
-function fromRemote(text){
-  console.log(text)
-}
-
-fromRemote("Funcao do repo remoto!")
 log("Test log");
