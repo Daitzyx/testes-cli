f832eec57c504abe9bfeeb65cbe2e77add5be9a5
Nathan <87097402+Daitzyx@users.noreply.github.com>
2024-08-02 10:29:50 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,4 +2,9 @@ function log(text){
   console.log(text);
 }
 
+function fromRemote(text){
+  console.log(text);
+}
+
 log("Testando chamada de função");
+fromRemote("Essa é uma funcão vindo do repositorio remoto");
